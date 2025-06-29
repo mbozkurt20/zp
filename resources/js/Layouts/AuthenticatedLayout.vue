e55d84c2a99671efed8d66e567a08cfe60b70096
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-06-29 13:40:08 +0300

    design

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -24,9 +24,7 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
                                 <Link :href="route('welcome')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
+                                    <img   class="block h-9 w-auto fill-current text-gray-800" src="/public/images/logo.png" alt="">
                                 </Link>
                             </div>
 
