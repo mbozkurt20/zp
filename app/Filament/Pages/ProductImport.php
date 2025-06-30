fb13db27c09643cb4bd0a109a0d581bd7abd1bc9
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-06-30 18:52:10 +0300

    excel product upload

---
+++ app/Filament/Pages/ProductImport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms;
+use Filament\Pages\Page;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use App\Imports\ProductExcelImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ProductImport extends Page implements Forms\Contracts\HasForms
+{
+    use Forms\Concerns\InteractsWithForms;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $title = 'Excel ile Ürün Yükle';
+    protected static string $view = 'filament.pages.product-import';
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('file')
+                    ->label('Excel Dosyası')
+                    ->disk('local')
+                    ->directory('imports')
+                    ->acceptedFileTypes(['application/vnd.ms-excel', '.xls'])
+                    ->required()
+            ])
+            ->statePath('data');
+    }
+
+    public function submit(): void
+    {
+        $file = reset($this->data['file']);
+        $path = $file->getPathname();
+
+        // dd($this->data['file']); // Debug için gerek kalmadı
+
+        try {
+            Excel::import(new ProductExcelImport(), $path);
+
+            Notification::make()
+                ->title('Başarılı!')
+                ->body('Ürünler başarıyla yüklendi.')
+                ->success()
+                ->send();
+        } catch (\Exception $e) {
+            Notification::make()
+                ->title('Hata!')
+                ->body('Yükleme sırasında hata oluştu: ' . $e->getMessage())
+                ->danger()
+                ->send();
+        }
+    }
+}
---
+++ app/Imports/ProductExcelImport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class ProductExcelImport implements ToCollection, WithHeadingRow
+{
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $index =>  $row) {
+            if ($index > 0){
+                $category = Category::where('name',$row['kategorisi'])->first();
+
+                $barcode = 'p-' . rand(100000000, 999999999);
+
+                if (Product::where('barcode', $barcode)->exists()) {
+                    $barcode = 'p-' . rand(100000000, 999999999);
+                }
+
+                if (!$category){
+                    $category = Category::create([
+                        'name' => $row['kategorisi'],
+                    ]);
+                }
+
+                Product::create([
+                    'name' => $row['urun_adi'], // başlık isimlerine göre
+                    'category_id' => $category->id,
+                    'barcode' => $barcode,
+                    'price' => $row['fiyat_tl'],
+                ]);
+            }
+        }
+    }
+}
