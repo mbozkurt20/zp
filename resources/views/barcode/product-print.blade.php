a7fbb6b9038e321ace375e71c4bc68166bae3493
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-07-02 13:57:39 +0300

    prduct barcode size changed

--- resources/views/barcode/product-print.blade.php
+++ resources/views/barcode/product-print.blade.php
@@ -3,22 +3,28 @@
 <head>
     <title>Barkod Yazdır</title>
     <style>
+        @page {
+            size: 50mm 30mm;
+            margin: 0;
+        }
         body {
+            margin: 0;
+            padding: 0;
             text-align: center;
-            padding: 50px;
+            /* Sayfa ortalama için */
+            width: 50mm;
+            height: 30mm;
         }
         img {
-            width: 300px;
-            height: auto;
+            width: 50mm;
+            height: 30mm;
+            object-fit: contain; /* Barkodun bozulmaması için */
         }
     </style>
 </head>
 <body onload="window.print();">
 
-
 <img src="https://bwipjs-api.metafloor.com/?bcid=code128&text={{ $product->barcode }}&includetext=false&scaleX=3&scaleY=0.8" alt="Barcode">
-
-
 
 </body>
 </html>
