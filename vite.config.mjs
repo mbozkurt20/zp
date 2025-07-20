b9bc1bfb8bf8bdb04eeda93ab05db3dd00bbb105
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-07-20 19:52:04 +0300

    crpyto

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { webcrypto } from 'crypto';
-
-// Eğer globalThis.crypto yoksa webcrypto'yu ata
-if (!globalThis.crypto) {
-    globalThis.crypto = webcrypto;
-}
 
 export default defineConfig({
     plugins: [
