da05a2002f9fdcf97004faf1903c28be6ddce2cb
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-07-23 21:50:00 +0300

    afs

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -132,6 +132,7 @@ const submitForm = async () => {
 
         toast.success('Müzik başarıyla yüklendi!')
 
+        tab.value = 'list'
         await fetchProducts();
     } catch (error) {
         console.error(error)
